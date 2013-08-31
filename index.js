0051842600ea44b32e97ff0e680c719ea80db714
bredele <olivier@petrofeed.com>
2013-08-31 13:17:13 -0600

    change plugin to set attributes like innerhtml, className, etc

--- index.js
+++ index.js
@@ -5,8 +5,8 @@
 
 module.exports = function(el, expr){
   var params = expr.split(',');
-  el.setAttribute(params[0], this.get(params[1]));
+  el[params[0]] = this.get(params[1]);
   this.on('change ' + params[1], function(val){
-    el.setAttribute(params[0], this.get(val));
+    el[params[0]] = this.get(val);
   });
 };
